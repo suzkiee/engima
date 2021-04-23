aed95a776bdccc1655279fd49a36b92ab6ad18a4
Suzie K <suziekim.dev@gmail.com>
2021-04-23 13:15:27 -0400

    Update file name.

--- lib/enigma_key.rb
+++ lib/enigma_key.rb
@@ -1,18 +1,17 @@
-class Key
-  attr_reader :key,
+class EnigmaKey
+  attr_reader :key_number,
               :date,
               :shifts
 
-  def initialize(key = 00000)
-    @key = key
+  def initialize(key_number = 00000)
+    @key_number = key_number
     @date = Date.today.strftime('%m/%d/%y')
     @shifts = []
   end
 
-  def generate_random_key
+  def generate_random_key_number
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     new_key = numbers.sample(5)
-    @key = new_key.join.to_i
-    require 'pry'; binding.pry
+    @key_number = new_key.join.to_i
   end
 end
