1d98f1b0c459d1b1c64091516103fccccd2f7291
Suzie K <suziekim.dev@gmail.com>
2021-04-25 21:14:45 -0400

    Change to class methods

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -10,21 +10,20 @@ class Algorithm
     @message = message
     @enigma_key = enigma_key
     @date = date 
-    @character_set = ('a'..'z').to_a << ' '
   end
 
-  def shift_forward(message, enigma_key, date)
-    ## maybe module?
+  def self.shift_forward(message, enigma_key, date)
     enigma_shift = EnigmaShift.new(enigma_key, date)
     enigma_shift.create_shifts(enigma_key, date)
     shifts = enigma_shift.shifts
+    character_set = ('a'..'z').to_a << ' '
     encrypted_string = ''
     downcased_string = message.downcase
 
     downcased_string.each_char.with_index do |char, index|
-      if @character_set.include?(char)
+      if character_set.include?(char)
         shifted_char_index = shifts[index % 4]
-        new_index = (@character_set.index(char) + shifted_char_index) % character_set.length
+        new_index = (character_set.index(char) + shifted_char_index) % character_set.length
         encrypted_string << character_set[new_index]
       else 
         encrypted_string << char
@@ -33,17 +32,18 @@ class Algorithm
     encrypted_string
   end
 
-  def shift_backward(encrypted_string, enigma_key, date)
+  def self.shift_backward(encrypted_string, enigma_key, date)
     enigma_shift = EnigmaShift.new(enigma_key, date)
     enigma_shift.create_shifts(enigma_key, date)
     shifts = enigma_shift.shifts
+    character_set = ('a'..'z').to_a << ' '
     original_message = ''
     
     encrypted_string.each_char.with_index do |char, index|
       if character_set.include?(char)
         shifted_char_index = shifts[index % 4]
-        new_index= (@character_set.index(char) - shifted_char_index) % character_set.length
-        original_message << @character_set[new_index]
+        new_index= (character_set.index(char) - shifted_char_index) % character_set.length
+        original_message << character_set[new_index]
       else
         original_message << char
       end
--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -3,14 +3,14 @@ require './lib/algorithm'
 require './lib/enigma_shift'
 
 describe Algorithm do
-  describe '#inistantian' do
+  describe '#initialize' do
     it 'exists' do
       algorithm = Algorithm.new('Hello World', '12345', '27/08/20')
 
       expect(algorithm).is_a? Algorithm 
     end
 
-    it 'has deafult attributes' do
+    xit 'has default attributes' do
       allow(Date).to receive(:today).and_return(Date.new(2020, 8, 27))
       algorithm = Algorithm.new('Hello World')
       expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
@@ -33,7 +33,7 @@ describe Algorithm do
   end
 
   describe '#character_set' do
-    it 'creates character set' do
+    xit 'creates character set' do
       algorithm = Algorithm.new('Hello World')
       expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
                   "j", "k", "l", "m", "n", "o", "p", "q", "r", 
@@ -45,29 +45,29 @@ describe Algorithm do
 
   describe '#shift_forward' do
     it 'shifts letters forward' do
-      algorithm = Algorithm.new('Hello World', '02715', '040895')
+      # algorithm = Algorithm.new('Hello World', '02715', '040895')
 
-      expect(algorithm.shift_forward('Hello World', '02715', '040895')).to eq 'keder ohulw'
+      expect(Algorithm.shift_forward('Hello World', '02715', '040895')).to eq 'keder ohulw'
     end
 
-    it 'returns character as itself if character not in chracter set' do
-      algorithm = Algorithm.new('Hello World!', '02715', '040895')
+    it 'encyrpts character as itself if character not in chracter set' do
+      # algorithm = Algorithm.new('Hello World!', '02715', '040895')
 
-      expect(algorithm.shift_forward('Hello World!', '02715', '040895')).to eq 'keder ohulw!'
+      expect(Algorithm.shift_forward('Hello World!', '02715', '040895')).to eq 'keder ohulw!'
     end
   end
 
   describe '#shift_backward' do
     it 'shifts letters backward' do
-      algorithm = Algorithm.new('Hello World', '02715', '040895')
+      # algorithm = Algorithm.new('Hello World', '02715', '040895')
 
-      expect(algorithm.shift_backward('keder ohulw', '02715', '040895')).to eq 'hello world'
+      expect(Algorithm.shift_backward('keder ohulw', '02715', '040895')).to eq 'hello world'
     end
 
-    it 'returns character as itself if character not in chracter set' do
-      algorithm = Algorithm.new('Hello World', '02715', '040895')
+    it 'decrypts character as itself if character not in chracter set' do
+      # algorithm = Algorithm.new('Hello World', '02715', '040895')
 
-      expect(algorithm.shift_backward('keder ohulw!', '02715', '040895')).to eq 'hello world!'
+      expect(Algorithm.shift_backward('keder ohulw!', '02715', '040895')).to eq 'hello world!'
     end
   end
 end
