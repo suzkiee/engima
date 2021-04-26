7f7348c825c060844e5bf7b61c7aef2425790576
Suzie K <suziekim.dev@gmail.com>
2021-04-26 19:07:45 -0400

    Remove attr_readers and instance variables

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,9 @@
 require './lib/algorithm'
 
 class Enigma
-  # attr_reader :message, 
-  #             :enigma_key,
-  #             :date
-
-  # def initialize
-  #   @message = message
-  # end
-
   def encrypt(message, enigma_key = '', date = Date.today.strftime('%d/%m/%y'))
-    enigma_key = EnigmaShift.random_key if enigma_key == ''
-    ciphertext = Algorithm.shift_forward(message, enigma_key, date) #class method?
+    enigma_key = random_key if enigma_key == ''
+    ciphertext = Algorithm.shift(message, enigma_key, date, 'encrypt') 
 
     encrpyted = {
       :encryption => ciphertext,
@@ -21,12 +13,16 @@ class Enigma
   end
 
   def decrypt(ciphertext, enigma_key = '', date = Date.today.strftime('%d/%m/%y'))
-    enigma_key = EnigmaShift.random_key if enigma_key == ''
-    original_message = Algorithm.shift_backward(ciphertext, enigma_key, date) #class method?
+    enigma_key = random_key if enigma_key == ''
+    original_message = Algorithm.shift(ciphertext, enigma_key, date, 'decrypt') 
     decrpyted = {
       :decryption => original_message,
       :key        => enigma_key,
       :date       => date.gsub('/','')
     }
+  end
+
+  def random_key
+    (0..9).to_a.sample(5).join
   end
 end
