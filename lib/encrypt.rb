bc534f8ac034209da5e0b954be94eef3ecd8415e
Suzie K <suziekim.dev@gmail.com>
2021-04-25 20:08:22 -0400

    Add runner files and txt files

---
+++ lib/encrypt.rb
@@ -0,0 +1,9 @@
+require 'date'
+require './lib/enigma'
+
+enigma = Enigma.new
+message = File.read(ARGV[0])
+encrypt = enigma.encrypt(message)
+File.write(ARGV[1], encrypt[:encryption])
+
+p "Created '#{ARGV[1]}' with key #{encrypt[:key]}and date #{encrypt[:date]}"
