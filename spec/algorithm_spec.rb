b939f47a5dfe3717d8686a1fe3555f4d8a2959dd
Suzie K <suziekim.dev@gmail.com>
2021-04-26 21:48:36 -0400

    Create tests for translate input method

--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -33,21 +33,62 @@ describe Algorithm do
     end
 
     it 'shifts letters backward' do
+      algorithm = Algorithm.new
       input = 'keder ohulw'
       enigma_key = '02715'
       date = '040895'
       crypt = 'decrypt'
 
-      expect(Algorithm.shift(input, enigma_key, date, crypt)).to eq 'hello world'
+      expect(algorithm.shift(input, enigma_key, date, crypt)).to eq 'hello world'
     end
 
     it 'decrypts character as itself if character not in character set' do
+      algorithm = Algorithm.new
       input = 'keder ohulw!'
       enigma_key = '02715'
       date = '040895'
       crypt = 'decrypt'
 
-      expect(Algorithm.shift(input, enigma_key, date, crypt)).to eq 'hello world!'
+      expect(algorithm.shift(input, enigma_key, date, crypt)).to eq 'hello world!'
+    end
+  end
+
+  describe '#create shifts' do
+    it 'creates shifts' do
+      algorithm = Algorithm.new
+
+      expect(algorithm.create_shifts('02715', '040895')).to eq ([3, 27, 73, 20])
+    end
+  end
+
+  describe '#character_set' do
+    it 'creates character set' do
+      algorithm = Algorithm.new
+      expected =["a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n", "o", "p", "q", "r", 
+                "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+      expect(algorithm.character_set).to eq expected
+    end
+  end
+
+  describe '#translate_input' do
+    it 'creates encrypted string output' do
+      algorithm = Algorithm.new
+      input = 'Hello World'
+      shifts = [3, 27, 73, 20]
+      crypt = 'encrypt'
+
+      expect(algorithm.translate_input(input, shifts, crypt)).to eq 'keder ohulw'
+    end
+
+    it 'creates decrypted string output' do
+      algorithm = Algorithm.new
+      input = 'Hello World'
+      shifts = [3, 27, 73, 20]
+      crypt = 'encrypt'
+
+      expect(algorithm.translate_input(input, shifts, crypt)).to eq 'hello world'
     end
   end
 end
