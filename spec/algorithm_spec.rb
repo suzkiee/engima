bb2a73ce866abe6c66ee33a52f44b3f1b093cd11
Suzie K <suziekim.dev@gmail.com>
2021-04-27 08:47:03 -0400

    Remove trailing whitespace

--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -64,9 +64,9 @@ describe Algorithm do
   describe '#character_set' do
     it 'creates character set' do
       algorithm = Algorithm.new
-      expected =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+      expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                  's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 
       expect(algorithm.character_set).to eq expected
     end
