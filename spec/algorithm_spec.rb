326af1800295b08bc104471ecbf0e0c609d29f5c
Suzie K <suziekim.dev@gmail.com>
2021-04-26 20:23:48 -0400

    Edit algorithm tests

--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -13,23 +13,39 @@ describe Algorithm do
   
   describe '::shift' do
     it 'shifts letters forward' do
+      message = 'Hello World'
+      enigma_key = '02175'
+      date = '040895'
+      crpyt = 'encrypt'
 
-      expect(Algorithm.shift('Hello World', '02715', '040895', 'encrypt')).to eq 'keder ohulw'
+      expect(Algorithm.shift(message, enigma_key, date, crypt)).to eq 'keder ohulw'
     end
 
     it 'encyrpts character as itself if character not in chracter set' do
+      message = 'Hello World!'
+      enigma_key = '02175'
+      date = '040895'
+      crpyt = 'encrypt'
 
-      expect(Algorithm.shift('Hello World!', '02715', '040895', 'encrypt')).to eq 'keder ohulw!'
+      expect(Algorithm.shift(message, enigma_key, date, crypt)).to eq 'keder ohulw!'
     end
     
     it 'shifts letters backward' do
+      ciphertext = 'keder ohulw'
+      enigma_key = '02175'
+      date = '040895'
+      crpyt = 'decrypt'
 
-      expect(Algorithm.shift('keder ohulw', '02715', '040895', 'decrypt')).to eq 'hello world'
+      expect(Algorithm.shift(ciphertext, enigma_key, date, crypt)).to eq 'hello world'
     end
     
     it 'decrypts character as itself if character not in character set' do
+      ciphertext = 'keder ohulw'
+      enigma_key = '02175'
+      date = '040895'
+      crpyt = 'decrypt'
 
-     expect(Algorithm.shift('keder ohulw!', '02715', '040895', 'decrypt')).to eq 'hello world!'
+      expect(Algorithm.shift(ciphertext, enigma_key, date, crypt)).to eq 'hello world!'
     end
   end
 end
