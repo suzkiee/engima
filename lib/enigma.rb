1c7c003c4f6249f9071e796bb67861d069f6b756
Suzie K <suziekim.dev@gmail.com>
2021-04-26 20:25:39 -0400

    Remove whitespace

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,7 +3,7 @@ require './lib/algorithm'
 class Enigma
   def encrypt(message, enigma_key = '', date = Date.today.strftime('%d/%m/%y'))
     enigma_key = random_key if enigma_key == ''
-    ciphertext = Algorithm.shift(message, enigma_key, date, 'encrypt') 
+    ciphertext = Algorithm.shift(message, enigma_key, date, 'encrypt')
     encrpyted = {
       :encryption => ciphertext,
       :key        => enigma_key,
@@ -13,7 +13,7 @@ class Enigma
 
   def decrypt(ciphertext, enigma_key = '', date = Date.today.strftime('%d/%m/%y'))
     enigma_key = random_key if enigma_key == ''
-    original_message = Algorithm.shift(ciphertext, enigma_key, date, 'decrypt') 
+    original_message = Algorithm.shift(ciphertext, enigma_key, date, 'decrypt')
     decrpyted = {
       :decryption => original_message,
       :key        => enigma_key,
